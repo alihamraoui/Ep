4fc4d3dc7413981fee09c883694a0601bbad5d26
Hamraoui Ali <72599648+alihamraoui@users.noreply.github.com>
2021-02-20 15:10:49 +0100

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -6,8 +6,10 @@ kb=450.000 #  kcal.mol-1.Å-2
 theteq= 104.5200 #°
 kt= 55.000 #kcal.mol-1.rad-1 
 #################################################################
+
 import math
 import sys
+
 coors = []
 # Open PDB file & get cartesian coordinates 
 with open(sys.argv[1], "r") as f:
@@ -36,12 +38,12 @@ def calc_angle( i,j,k):
         # d1(d1x, d1y, d1z) and d2(d2x, d2y, d2z) are two Vector make an theta angle
         r1, d1x, d1y, d1z =calc_dist(i,j)
         r2, d2x, d2y, d2z =calc_dist(i,k)
-        #norme de vecteur d1 et d2
+        #norm of vectors d1 & d2
         nor_d1= math.sqrt(d1x*d1x + d1y*d1y + d1z*d1z)
         nor_d2= math.sqrt(d2x*d2x + d2y*d2y + d2z*d2z)
-        #scalaireProdact
+        #scalaire Prodact of d1 X d2
         ij_jk= d1x*d2x + d1y*d2y + d1z*d2z
-        # Calc angle teta
+        # Calc angle theta
         thet=math.acos((ij_jk / (nor_d1 * nor_d2)))
         return math.degrees(thet) #get angle in degree
 
